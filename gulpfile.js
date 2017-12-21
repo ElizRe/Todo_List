23f369ee0c1c9ac6c53fa007331d45be80647618
ElizRe <elizreed@laposte.net>
2017-12-21 12:27:29 +0100

    correct gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var browserSync =require("browser-sync"),
+var browserSync=require("browser-sync"),
     gulp=require("gulp"),
     gulpCleanCss =require("gulp-clean-css"),
     gulpSass=require("gulp-sass"),
@@ -6,16 +6,16 @@ var browserSync =require("browser-sync"),
     mergeStream=require("merge-stream");
 
 gulp.task("styleone",function(){
-    gulp.src("/node_modules/materialize-css/dist/css/materialize.min.cssb")
-        .pipe(gulp.dest("/dist/css/"));       
+    gulp.src("./node_modules/materialize-css/dist/css/materialize.min.cssb")
+        .pipe(gulp.dest("./dist/css/"));       
 
 });
 
 gulp.task("scriptjs",function() {
-    var getjq = gulp.src("/node_modules/jquery/dist/jquery.min.js")
-                .pipe(gulp.dest("/dist/js/"));
-    var getmatz=gulp.src("/node_modules/materialize-css/dist/js/materialize.min.js")
-                .pipe(gulp.dest("dist/js/"));
+    var getjq = gulp.src("./node_modules/jquery/dist/jquery.min.js")
+                .pipe(gulp.dest("./dist/js/"));
+    var getmatz=gulp.src("./node_modules/materialize-css/dist/js/materialize.min.js")
+                .pipe(gulp.dest("./dist/js/"));
 
     return mergeStream(getjq,getmatz);
 
@@ -25,14 +25,14 @@ gulp.task("sass",function() {
     return gulp.src("./assets/scss/*.scss")
     .pipe(gulpSass())
     .pipe(gulpCleanCss())
-    .pipe(gulp.dest("/dist/css/"));
+    .pipe(gulp.dest("./dist/css/"));
 
 });
 
 gulp.task("uglify",function() {
-    gulp.src("/assets/js/*.js")
+    gulp.src("./assets/js/*.js")
     .pipe(gulpUglify())
-    .pipe(gulp.dest("/dist/js/"));
+    .pipe(gulp.dest("./dist/js/"));
 })
 
 gulp.task("browser-sync",function () {
@@ -42,8 +42,8 @@ gulp.task("browser-sync",function () {
 });
 
 gulp.task("gulpwatch",['styleone','scriptjs','sass','uglify'],function () {
-        gulp.watch("/assets/scss/*.scss",["sass"]).on("change", browserSync.reload)
-        gulp.watch("/assets/js/*.js",["uglify"]).on("change", browserSync.reload)
+        gulp.watch("./assets/scss/*.scss",["sass"]).on("change", browserSync.reload)
+        gulp.watch("./assets/js/*.js",["uglify"]).on("change", browserSync.reload)
         gulp.watch("./*.html", ).on("change", browserSync.reload)
 
     });
